a494f5ec4dcb3cbe503c530b57da423f54a149d9
Alex <221934805+Alex5724@users.noreply.github.com>
2025-11-27 16:32:37 +0200

    Added extra configuration to webpack and eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,23 @@
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   {
     files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
     plugins: {
       prettier: eslintPluginPrettier,
     },
     rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
       'prettier/prettier': 'error',
     },
+  },
+  {
+    rules: eslintConfigPrettier.rules,
   },
 ];
